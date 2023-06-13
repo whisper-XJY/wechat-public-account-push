8e0184f78c362cce17e40e60afcf260a4f8f3ba4
whisper-XJY <136424855+whisper-XJY@users.noreply.github.com>
2023-06-13 14:46:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oygu66IcGjOKPupyGkwFUDNGUn7g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'X3-RoEbV_kgpdS3G4TMmnoXmXAbwun_MSYHQHVKUDao',
+      useTemplateId: '	UwePw0-Iro3HK1Jcoj-gKJ7p5SUqYNoJQuZo1rtIpeQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-03',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'X3-RoEbV_kgpdS3G4TMmnoXmXAbwun_MSYHQHVKUDao',
+  CALLBACK_TEMPLATE_ID: '	UwePw0-Iro3HK1Jcoj-gKJ7p5SUqYNoJQuZo1rtIpeQ',
 
   CALLBACK_USERS: [
     {
